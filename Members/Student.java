4d2c846108cd170bad7620ec19f1293987389419
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-04-30 20:36:23 +0100

    adding student class

---
+++ Members/Student.java
@@ -0,0 +1,6 @@
+public class Student extends person{
+    public Student(String matricule,String first_name,String last_name){
+        super(matricule, last_name, first_name, "Student");
+    } 
+       
+}
--- Members/person.java
+++ Members/person.java
@@ -1,3 +1,25 @@
 public class person{
-        
+    private String matricule;
+    private String first_name;
+    private String last_name;    
+    private String role;
+    public person(String matricule,String last_name,String first_name,String role){
+        this.matricule=matricule;
+        this.last_name=last_name;
+        this.first_name=first_name;
+        this.role=role;
+    }
+    public String getmatricule(){
+        return matricule;
+    }
+    public String getlastname(){
+        return last_name;
+    }
+    public String getfirst_name(){
+        return first_name;
+    }
+    public String role(){
+        return role;
+    }     
+    
 }
